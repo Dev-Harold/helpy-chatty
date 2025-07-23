d8fc759ab0e86a2898eb92514ff9bc1fa5908a59
Hugo <hugo.clavel@gmail.com>
2025-07-23 11:37:52 -0700

    Redid home page navigation per spec

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -51,7 +51,6 @@ export default function Home() {
 
   const handleSubmit = (e: React.FormEvent) => {
     e.preventDefault()
-    console.log('Message submitted:', message)
     const chatId = uuidv4()
     const encodedMessage = encodeURIComponent(message)
     router.push(`/chat/${chatId}?message=${encodedMessage}`)
@@ -69,45 +68,39 @@ export default function Home() {
       
       <main className="pt-[30px] px-4 min-h-screen bg-gray-100">
         <div className="max-w-7xl mx-auto">
-          <div className="bg-white rounded-lg shadow-lg p-6 mb-4">
-            <div className="flex flex-col lg:flex-row items-center justify-between">
-              <div className="text-center flex-1 mb-6 lg:mb-0">
-                <h2 className="text-2xl font-bold text-gray-800 mb-2 md:text-4xl mb-8 md:mb-16">Quick, Easy, & Free Tech Support.</h2>
-                <h2 className="text-2xl font-bold text-gray-800 mb-2 md:text-4xl">Online assistance for ANY of your devices.</h2>
-              </div>
-              <div className="image-container block flex-shrink-0 lg:ml-6 m-1">
-                <Image 
-                  src="/Electronics.jpeg" 
-                  alt="Tech Support" 
-                  width={750}
-                  height={750}
-                  className="rounded-lg"
-                />
-              </div>
+          <div className="bg-white rounded-lg shadow-lg mb-4 flex flex-col lg:flex-row items-center justify-center overflow-hidden">
+            <div className="text-center flex-1 min-h-[300px] md:min-h-[500px] bg-cover bg-center bg-no-repeat relative flex items-center" style={{ backgroundImage: 'url(/Electronics.jpeg)' }}>
+             <div className="absolute inset-0 bg-black/70 md:bg-black/0 md:bg-gradient-to-r md:from-black md:via-black/70 md:to-transparent"></div>
+             <div className="relative z-10 p-6 md:max-w-[50%] flex flex-col items-center justify-center">
+              <h2 className="text-2xl font-bold text-white mb-2 md:text-4xl mb-8 md:mb-16">Quick, Easy, & Free Tech Support.</h2>
+              <h2 className="text-2xl font-bold text-white mb-2 md:text-4xl">Online assistance for ANY of your devices.</h2>
             </div>
           </div>
+          <div className="flex flex-col gap-4 items-center min-w-[40%] p-6">
+            <h1 className="text-lg italic  md:text-2xl text-gray-800 mb-2 text-center">Describe your problem to get started:</h1>
+            <div className="flex-1 w-full flex">
+              <textarea
+                ref={textareaRef}
+                value={message}
+                onChange={(e) => setMessage(e.target.value)}
+                placeholder="Write your problem here..."
+                className="w-full md:min-h-[250px] p-3 border-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-sm md:text-lg resize-none leading-[28px]"
+                required
+                rows={1}
+              />
+            </div>
+            <button 
+              type="submit"
+              className="w-full bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md transition-colors text-sm md:text-lg font-semibold whitespace-nowrap h-full md:mt-12"
+              aria-label="Send message"
+            >
+              Chat Now
+            </button>
+          </div>
+        </div>
 
           <form onSubmit={handleSubmit} className="bg-white rounded-lg shadow-lg p-6">
-            <div className="flex flex-col md:flex-row gap-4 items-center">
-              <div className="flex-1 w-full flex">
-                <textarea
-                  ref={textareaRef}
-                  value={message}
-                  onChange={(e) => setMessage(e.target.value)}
-                  placeholder="Describe your problem to get started..."
-                  className="w-full p-3 border-2 border-gray-300 rounded-md focus:outline-none focus:border-blue-500 text-sm md:text-lg resize-none leading-[28px]"
-                  required
-                  rows={1}
-                />
-              </div>
-              <button 
-                type="submit"
-                className="md:w-auto w-full bg-blue-500 hover:bg-blue-600 text-white p-3 rounded-md transition-colors text-sm md:text-lg font-semibold whitespace-nowrap h-full"
-                aria-label="Send message"
-              >
-                Chat Now
-              </button>
-            </div>
+            
             
             <div className="mt-4">
               <h1 className="text-lg italic  md:text-2xl text-gray-800 mb-2 text-center">Or choose from a common problem below:</h1>
